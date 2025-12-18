18b3369022d163b19e0b69490f930a575ada4219
Coorv <119966974+Coorv@users.noreply.github.com>
2025-12-18 11:25:24 +0200

    Kordamis ülesanded

---
+++ IF_loogilisedoperaatorid_tingimus/IF-ELSEIF-ELSE/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace IF_ELSEIF_ELSE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //küsi kasutajalt tema pikkust
+            //kui ta on lühem kui 0cm, ütle võimatu pikkus
+            //kui ta on lühem kui 1m, ütle juntsu
+            //kui ta on lühem kui 2m, ütle tavaline kolge
+            //kui ta on pikem kui 2m, ütle hiiglane
+
+            //küsi kasutaja vanust
+            //küsi kasutajalt praegust aastaarvu
+            //lahuta aastast kasutaja vanus
+            //kui on sündinud 2025-2020, ütle talle kes sulle arvuti juba andis
+            //kui on sündinud 2020-2015, ütle talle skibidi sigmaless
+            //kui on sündinud 2015-2010, küsi talt kas ta tahab saada programmeerijaks
+            //  kui jah, ütle tubli
+            //  kui ei, ütle kahju
+            //kui sündinud enne 2010 küsi mitu range comicsit ta oma redditi lugemise ajal leidis
+
+            //küsi kasutajalt elukoha suunakoodi
+            //kui suunakood on väiksem kui 5 tähte, ütle talle 1 täht on puudu
+            //kui suunakood on suurem kui 5 tähte, ütle talle 1 täht on üle
+            //kui suunakood on 5 tähte, ütle "ahha nüüd tean kus elad"
+
+            //küsi kasutajalt kas ta töötab
+            //kui kasutaja vastab jah, siis ütle tubli
+            //kui kasutaja vastab ei, siis ütle kahju, miks sa siis töökohta ei otsi?
+            //kui kasutaja vastab otsin siis ütle loodan et sa saad varsti tööle C:
+
+            //küsi kasutajalt tema parooli 
+            //seejärel küsi kasutajalt tema 2FA koodi,
+            //kui parool on vale, ütle sissepääs keelatud
+            //kui parool on õige, kontrolli 2FA koodi:
+            //  kui 2FA kood on lühem kui 6 tähte ütle sissepääs on keelatud, kood on liiga lühike
+            //  kui 2FA kood on pikem kui 6 tähte ütle sissepääs on keelatud, kood on liiga pikk
+            //  kui 2FA kood on 6 tähte lase sisse, ütle "oled sissepääsenud"
+            //NB: 2FA kood ei matchima eksisteeriva näitega nagu parool.
+        }
+    }
+}
